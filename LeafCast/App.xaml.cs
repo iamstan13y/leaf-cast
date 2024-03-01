657d3b8193945e1dc6727d7742797b2a447fa9bf
Keith Stanley <iamstan13y@gmail.com>
2024-03-01 02:52:49 +0200

    Add project files.

---
+++ LeafCast/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeafCast
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LeafCast/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LeafCast
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
