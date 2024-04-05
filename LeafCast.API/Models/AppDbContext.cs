6a96b3792d6158dc9bbbd9f9e8fb846b73ab516f
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 00:38:03 +0200

    TobaccoGrade Model & DB Migrations.

--- LeafCast.API/Models/AppDbContext.cs
+++ LeafCast.API/Models/AppDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LeafCast.API.Models.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeafCast.API.Models;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<TobaccoGrade>? TobaccoGrades { get; set; }
 }
