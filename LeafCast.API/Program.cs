5cd1617e7b238c147a1aa9ae7a29fe84bd1f1e60
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 01:48:14 +0200

    Prediction Repository Pattern Config.

--- LeafCast.API/Program.cs
+++ LeafCast.API/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ITobaccoGradeRepository, TobaccoGradeRepository>();
+builder.Services.AddScoped<IPredictionRepository, PredictionRepository>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
