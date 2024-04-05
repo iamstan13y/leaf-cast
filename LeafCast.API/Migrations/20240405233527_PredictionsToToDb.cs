dd9d679332a08dad69b6942eabd725a594ae4edb
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 01:39:10 +0200

    Prediction Model & DB Migrations.

---
+++ LeafCast.API/Migrations/20240405233527_PredictionsToToDb.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeafCast.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PredictionsToToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Predictions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TobaccoGradeId = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Predictions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Predictions_TobaccoGrades_TobaccoGradeId",
+                        column: x => x.TobaccoGradeId,
+                        principalTable: "TobaccoGrades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Predictions_TobaccoGradeId",
+                table: "Predictions",
+                column: "TobaccoGradeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Predictions");
+        }
+    }
+}
