d61a77c06b67d6a41ad519eea3b965a8d937c134
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 01:45:14 +0200

    Tobacco Grade Repository Pattern Config.

--- LeafCast.API/Program.cs
+++ LeafCast.API/Program.cs
@@ -1,4 +1,5 @@
 using LeafCast.API.Models;
+using LeafCast.API.Models.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -8,6 +9,8 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ITobaccoGradeRepository, TobaccoGradeRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
