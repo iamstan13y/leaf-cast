8ecf0364fa6520e260bcb1a6485e1d352a3a7f61
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 01:39:24 +0200

    Prediction Model & DB Migrations.

--- LeafCast.API/Models/AppDbContext.cs
+++ LeafCast.API/Models/AppDbContext.cs
@@ -6,4 +6,5 @@ namespace LeafCast.API.Models;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<TobaccoGrade>? TobaccoGrades { get; set; }
+    public DbSet<Prediction>? Predictions { get; set; }
 }
