725251edb2d57a54639efa766e498322a5198f79
Keith Stanley <iamstan13y@gmail.com>
2024-04-06 01:39:22 +0200

    Prediction Model & DB Migrations.

---
+++ LeafCast.API/Models/Data/Prediction.cs
@@ -0,0 +1,10 @@
+﻿namespace LeafCast.API.Models.Data;
+
+public class Prediction
+{
+    public int Id { get; set; }
+    public int TobaccoGradeId { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public TobaccoGrade? TobaccoGrade { get; set; }
+}
