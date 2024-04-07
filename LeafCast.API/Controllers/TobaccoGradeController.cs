cbc6397ade3ce0bffae6fb46ff7d72cb820d1e0c
Keith Stanley <iamstan13y@gmail.com>
2024-04-07 09:08:20 +0200

    Tobacco Grade Controller -WIP.

---
+++ LeafCast.API/Controllers/TobaccoGradeController.cs
@@ -0,0 +1,14 @@
+﻿using LeafCast.API.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeafCast.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class TobaccoGradeController(ITobaccoGradeRepository repository) : ControllerBase
+{
+    private readonly ITobaccoGradeRepository _repository = repository;
+
+    [HttpGet]
+    public async Task<IActionResult> Get() => Ok(await _repository.AddRangeAsync)
+}
