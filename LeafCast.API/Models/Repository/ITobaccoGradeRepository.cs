8b3f0559ae944a0d6d4a53654a35b2673d1d763d
Keith Stanley <iamstan13y@gmail.com>
2024-04-07 09:08:24 +0200

    Tobacco Grade Controller -WIP.

--- LeafCast.API/Models/Repository/ITobaccoGradeRepository.cs
+++ LeafCast.API/Models/Repository/ITobaccoGradeRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace LeafCast.API.Models.Repository;
+﻿using LeafCast.API.Models.Data;
+
+namespace LeafCast.API.Models.Repository;
 
 public interface ITobaccoGradeRepository
 {
+    Task<bool> AddRangeAsync(List<TobaccoGrade> tobaccoGrades);
 }
