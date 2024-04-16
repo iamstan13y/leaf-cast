9800d62d3ed6413a5bea95d9f11791e654bcfbbb
Keith Stanley <iamstan13y@gmail.com>
2024-04-17 00:22:31 +0200

    Implemented Add Bulk Records.

--- LeafCast.API/Models/Repository/PredictionRepository.cs
+++ LeafCast.API/Models/Repository/PredictionRepository.cs
@@ -1,5 +1,16 @@
-﻿namespace LeafCast.API.Models.Repository;
+﻿using LeafCast.API.Models.Data;
 
-public class PredictionRepository : IPredictionRepository
+namespace LeafCast.API.Models.Repository;
+
+public class PredictionRepository(AppDbContext context) : IPredictionRepository
 {
+    private readonly AppDbContext _context = context;
+
+    public async Task<bool> AddBulkAsync(IEnumerable<Prediction> predictions)
+    {
+        await _context.Predictions!.AddRangeAsync(predictions);
+        await context.SaveChangesAsync();
+        
+        return true;
+    }
 }
