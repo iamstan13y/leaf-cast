6391d56217d902b52a0de7484305968228f8a310
Keith Stanley <iamstan13y@gmail.com>
2024-04-18 01:43:18 +0200

    Some Work in Progress.

--- LeafCast.API/Controllers/TobaccoGradeController.cs
+++ LeafCast.API/Controllers/TobaccoGradeController.cs
@@ -1,4 +1,5 @@
-﻿using LeafCast.API.Models.Repository;
+﻿using LeafCast.API.Models.Local;
+using LeafCast.API.Models.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LeafCast.API.Controllers;
@@ -10,5 +11,11 @@ public class TobaccoGradeController(ITobaccoGradeRepository repository) : Contro
     private readonly ITobaccoGradeRepository _repository = repository;
 
     [HttpPost("bulk")]
-    public async Task<IActionResult> Get() => Ok
+    public async Task<IActionResult> Post(List<GradeRequest> request)
+    {
+        var result = await _repository.AddBulkAsync(request);
+        if (!result.Success) return BadRequest(result);
+
+        return Ok(result);
+    }
 }
