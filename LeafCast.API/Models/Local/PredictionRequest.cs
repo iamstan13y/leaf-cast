0a2d2ff6b3335de634daf855761d256573be50dd
Keith Stanley <iamstan13y@gmail.com>
2024-04-18 01:43:25 +0200

    Some Work in Progress.

---
+++ LeafCast.API/Models/Local/PredictionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LeafCast.API.Models.Local;
+
+public class PredictionRequest
+{
+    public int TobaccoGradeId { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+}
