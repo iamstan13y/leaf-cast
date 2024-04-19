ee3a113fd0ec1cd714492ceeea55e3737bafb122
Keith Stanley <iamstan13y@gmail.com>
2024-04-20 00:09:39 +0200

    Tobacco Grade Endpoints.

--- LeafCast.API/Models/Repository/ITobaccoGradeRepository.cs
+++ LeafCast.API/Models/Repository/ITobaccoGradeRepository.cs
@@ -6,4 +6,6 @@ namespace LeafCast.API.Models.Repository;
 public interface ITobaccoGradeRepository
 {
     Task<Result<bool>> AddBulkAsync(List<GradeRequest> tobaccoGrades);
+    Task<Result<IEnumerable<TobaccoGrade>>> GetAllAsync();
+    Task<Result<TobaccoGrade>> GetByIdAsync(int id);
 }
