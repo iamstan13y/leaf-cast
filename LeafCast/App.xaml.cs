e4499e06e15860ff8146eedafa4bcb051e98e42e
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 17:46:48 +0200

    Some Dashboard WIP.

--- LeafCast/App.xaml.cs
+++ LeafCast/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new SplashScreenPage());
+            MainPage = new NavigationPage(new DashboardPage());
         }
     }
 }
