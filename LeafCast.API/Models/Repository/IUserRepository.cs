162864210fb12e614f7b401f791541e43051c0a2
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 15:30:11 +0200

    User Stuff.

---
+++ LeafCast.API/Models/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using LeafCast.API.Models.Data;
+using LeafCast.API.Models.Local;
+
+namespace LeafCast.API.Models.Repository;
+
+public interface IUserRepository
+{
+    Task<Result<User>> LoginAsync(LoginRequest request);
+    Task<Result<User>> AddAsync(User user);
+    //Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePasswordRequest);
+    //Task<Result<string>> GetResetPasswordCodeAsync(string email);
+    //Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPasswordRequest);
+    //Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccountRequest);
+    //Task<Result<string>> ResendOtpAsync(string email);
+}
