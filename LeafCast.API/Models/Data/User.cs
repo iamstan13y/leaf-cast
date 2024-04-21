2f5c1418b0aa51dccbee321517f08775234d8d24
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 15:29:57 +0200

    User Stuff.

---
+++ LeafCast.API/Models/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeafCast.API.Models.Data;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public bool IsActive { get; set; } = false;
+    [NotMapped]
+    public string? Token { get; set; }
+}
