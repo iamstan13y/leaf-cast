a89bced1811befa666e87744503dc29dcaafe5a6
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 15:29:53 +0200

    User Stuff.

---
+++ LeafCast.API/Models/Data/OtpCode.cs
@@ -0,0 +1,8 @@
+﻿namespace LeafCast.API.Models.Data;
+
+public class OtpCode
+{
+    public int Id { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Code { get; set; }
+}
