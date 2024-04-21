e89e0ae61eeb4100e1e52a0591371d87297490c1
Keith Stanley <iamstan13y@gmail.com>
2024-04-22 00:53:34 +0200

    Profile Settings Page.

--- LeafCast/App.xaml.cs
+++ LeafCast/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new PredictionsPage());
+            MainPage = new NavigationPage(new ProfileSettingsPage());
         }
     }
 }
