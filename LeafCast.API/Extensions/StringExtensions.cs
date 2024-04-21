5f202425d6d21b0f4276ccad4dc5823c6413257c
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 16:56:31 +0200

    User Controller & tumwewo tu stuff twakakosha.

---
+++ LeafCast.API/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace LeafCast.API.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToZimMobileNumber(this string mobileNumber)
+    {
+        mobileNumber = mobileNumber.TrimStart('0');
+
+        return $"263{mobileNumber}";
+    }
+}
