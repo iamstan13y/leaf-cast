73a5209a6a81a004ebe202ca0daa42ff586ec648
Keith Stanley <iamstan13y@gmail.com>
2024-04-21 15:30:50 +0200

    User Stuff.

---
+++ LeafCast.API/Services/SmsService.cs
@@ -0,0 +1,35 @@
+﻿
+using LeafCast.API.Models.Local;
+using RestSharp;
+
+namespace LeafCast.API.Services;
+
+public class SmsService(IConfiguration configuration) : ISmsService
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public async Task<bool> SendAsync(SmsRequest smsRequest)
+    {
+        var options = new RestClientOptions(_configuration["SmsService:BaseUrl"]!)
+        {
+            MaxTimeout = -1,
+        };
+
+        var client = new RestClient(options);
+        var request = new RestRequest(_configuration["SmsService:Resource"]!, Method.Post);
+        request.AddHeader("Authorization", _configuration["SmsService:ApiKey"]!);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Accept", "application/json");
+
+        var body = string.Format(_configuration["SmsService:Message"]!, smsRequest.PhoneNumber, _configuration["SmsService:Sender"]!, smsRequest.Message);
+
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        
+        if(response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+        return false;
+    }
+}
