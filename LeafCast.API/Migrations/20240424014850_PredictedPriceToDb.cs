d51e8dae0a83fccebb3fe10dc30ba0c9b041e193
Keith Stanley <iamstan13y@gmail.com>
2024-04-24 04:18:39 +0200

    Prediction Model Fix & DB Migrations.

---
+++ LeafCast.API/Migrations/20240424014850_PredictedPriceToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeafCast.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PredictedPriceToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Predictions",
+                newName: "PredictedPrice");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualPrice",
+                table: "Predictions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActualPrice",
+                table: "Predictions");
+
+            migrationBuilder.RenameColumn(
+                name: "PredictedPrice",
+                table: "Predictions",
+                newName: "Price");
+        }
+    }
+}
