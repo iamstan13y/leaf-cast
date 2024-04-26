28fe05106a1b12d4c877a8ee99696549de1fdffe
Keith Stanley <iamstan13y@gmail.com>
2024-04-26 04:43:27 +0200

    Stuff.

---
+++ LeafCast/Models/Data/Prediction.cs
@@ -0,0 +1,11 @@
+﻿namespace LeafCast.Models.Data;
+
+public class Prediction
+{
+    public int Id { get; set; }
+    public int TobaccoGradeId { get; set; }
+    public int Year { get; set; }
+    public decimal ActualPrice { get; set; }
+    public decimal PredictedPrice { get; set; }
+    public TobaccoGrade? TobaccoGrade { get; set; }
+}
