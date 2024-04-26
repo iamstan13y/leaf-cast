8bba996575619380391a19733d2cead632a1468b
Keith Stanley <iamstan13y@gmail.com>
2024-04-26 04:43:49 +0200

    Stuff.

--- LeafCast/NavBar.xaml.cs
+++ LeafCast/NavBar.xaml.cs
@@ -5,5 +5,29 @@ public partial class NavBar : ContentView
 	public NavBar()
 	{
 		InitializeComponent();
-	}
+
+        var viewMoreGestureRecognizer = new TapGestureRecognizer();
+        viewMoreGestureRecognizer.Tapped += async (s, e) =>
+        {
+            await Navigation.PushAsync(new ProfileSettingsPage());
+        };
+
+        profileTab.GestureRecognizers.Add(viewMoreGestureRecognizer);
+
+        var statsGestureRecognizer = new TapGestureRecognizer();
+        statsGestureRecognizer.Tapped += async (s, e) =>
+        {
+            await Navigation.PushAsync(new PredictionsPage());
+        };
+
+        statsTab.GestureRecognizers.Add(statsGestureRecognizer);
+
+        var dashGestureRecognizer = new TapGestureRecognizer();
+        dashGestureRecognizer.Tapped += async (s, e) =>
+        {
+            await Navigation.PushAsync(new DashboardPage());
+        };
+
+        dashTab.GestureRecognizers.Add(dashGestureRecognizer);
+    }
 }
