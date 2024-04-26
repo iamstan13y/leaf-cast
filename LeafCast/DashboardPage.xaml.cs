f81463518630e214506bc36005b7270989058c7f
Keith Stanley <iamstan13y@gmail.com>
2024-04-26 02:08:38 +0200

    Dashboard Another Phase Done.

--- LeafCast/DashboardPage.xaml.cs
+++ LeafCast/DashboardPage.xaml.cs
@@ -1,9 +1,35 @@
+using LeafCast.Services;
+using LeafCast.ViewModels;
+using System.ComponentModel;
+
 namespace LeafCast;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage()
+    private async Task InitializeAsync()
+    {
+        var httpClient = new HttpClient();
+        var httpService = new HttpService(httpClient);
+        var viewModel = new TopGradesViewModel
+        {
+            HttpService = httpService
+        };
+
+        await viewModel.LoadDataAsync();
+        BindingContext = viewModel;
+    }
+
+    public DashboardPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        InitializeAsync().ConfigureAwait(false);
+
+        var viewMoreGestureRecognizer = new TapGestureRecognizer();
+        viewMoreGestureRecognizer.Tapped += async (s, e) =>
+        {
+            await Navigation.PushAsync(new StatsPage());
+        };
+
+        viewMoreButton.GestureRecognizers.Add(viewMoreGestureRecognizer);
+    }
 }
