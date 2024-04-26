e8c5525ff40dacc4b3b19061755b1c7376d159dc
Keith Stanley <iamstan13y@gmail.com>
2024-04-26 02:08:00 +0200

    Dashboard Another Phase Done.

---
+++ LeafCast/Models/Local/CreateAccountRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LeafCast.Models.Local;
+
+public class CreateAccountRequest
+{
+    public string? PhoneNumber { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+}
