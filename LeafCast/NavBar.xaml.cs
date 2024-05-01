afb9fa61dca7c7934b3cb7670c2944af5d8a949c
Keith Stanley <iamstan13y@gmail.com>
2024-05-01 23:06:58 +0200

    Exit Button Stuff.

--- LeafCast/NavBar.xaml.cs
+++ LeafCast/NavBar.xaml.cs
@@ -2,9 +2,9 @@ namespace LeafCast;
 
 public partial class NavBar : ContentView
 {
-	public NavBar()
-	{
-		InitializeComponent();
+    public NavBar()
+    {
+        InitializeComponent();
 
         var viewMoreGestureRecognizer = new TapGestureRecognizer();
         viewMoreGestureRecognizer.Tapped += async (s, e) =>
@@ -29,5 +29,28 @@ public partial class NavBar : ContentView
         };
 
         dashTab.GestureRecognizers.Add(dashGestureRecognizer);
+
+        var xGestureRecognizer = new TapGestureRecognizer();
+        xGestureRecognizer.Tapped += async (s, e) =>
+        {
+            bool answer = await Application.Current.MainPage.DisplayAlert("Exit", "Are you sure you want to exit the app?", "Yes", "No");
+            if (answer)
+            {
+              
+
+                // OR
+
+                // Terminate the process
+                 Environment.Exit(0);
+
+                // OR
+
+                // Call platform-specific code to exit the app
+                // DependencyService.Get<IExitAppService>().ExitApp();
+            }
+
+        };
+
+        x.GestureRecognizers.Add(xGestureRecognizer);
     }
 }
