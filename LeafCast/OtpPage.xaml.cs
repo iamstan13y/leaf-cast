3ab62355a4c4e82de9d39e4a65641cd77d7afb6e
Keith Stanley <iamstan13y@gmail.com>
2024-05-02 13:24:29 +0200

    Account Creation Stuff.

---
+++ LeafCast/OtpPage.xaml.cs
@@ -0,0 +1,32 @@
+using LeafCast.Services;
+
+namespace LeafCast;
+
+public partial class OtpPage : ContentPage
+{
+    private readonly HttpService _httpService;
+    public OtpPage()
+    {
+        InitializeComponent();
+
+        var httpClient = new HttpClient();
+        var httpService = new HttpService(httpClient);
+        
+        _httpService = httpService;
+
+        signUpButton.Clicked += SignUpButton_Clicked;
+    }
+
+    private async void SignUpButton_Clicked(object? sender, EventArgs e)
+    {
+        activityIndicator.IsRunning = true;
+        activityIndicator.IsVisible = true;
+
+        activityIndicator.IsRunning = false;
+        activityIndicator.IsVisible = false;
+
+        await DisplayAlert("Success", "Account successfully verified", "OK");
+        await Navigation.PushAsync(new LoginPage());
+
+    }
+}
