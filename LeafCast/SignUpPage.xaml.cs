70c89199fad98cd9bb27154c0401f03c26b32b0c
Keith Stanley <iamstan13y@gmail.com>
2024-05-02 13:24:35 +0200

    Account Creation Stuff.

--- LeafCast/SignUpPage.xaml.cs
+++ LeafCast/SignUpPage.xaml.cs
@@ -1,10 +1,19 @@
+using LeafCast.Models.Local;
+using LeafCast.Services;
+
 namespace LeafCast;
 
 public partial class SignUpPage : ContentPage
 {
-	public SignUpPage()
-	{
+    private readonly HttpService _httpService;
+    public SignUpPage()
+    {
         InitializeComponent();
+
+        var httpClient = new HttpClient();
+        var httpService = new HttpService(httpClient);
+        
+        _httpService = httpService;
 
         var loginUpTapGestureRecognizer = new TapGestureRecognizer();
         loginUpTapGestureRecognizer.Tapped += async (s, e) =>
@@ -13,5 +22,36 @@ public partial class SignUpPage : ContentPage
         };
 
         loginLabel.GestureRecognizers.Add(loginUpTapGestureRecognizer);
+
+        signUpButton.Clicked += SignUpButton_Clicked;
+    }
+
+    private async void SignUpButton_Clicked(object? sender, EventArgs e)
+    {
+        activityIndicator.IsRunning = true;
+        activityIndicator.IsVisible = true;
+
+        var signUpData = new CreateAccountRequest
+        {
+            FullName = fullName.Text,
+            PhoneNumber = phoneNumber.Text,
+            UserName = username.Text,
+            Password = password.Text
+        };
+
+        var response = await _httpService.CreateAccountAsync(signUpData);
+
+        activityIndicator.IsRunning = false;
+        activityIndicator.IsVisible = false;
+
+        if (response.Success)
+        {
+            await DisplayAlert("Success", "Account created successfully", "OK");
+            await Navigation.PushAsync(new OtpPage());
+        }
+        else
+        {
+            await DisplayAlert("Error", "Account creation failed", "OK");
+        }
     }
 }
