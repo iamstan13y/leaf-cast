5bd0b6373e90eebc9149ebb9a5ef7736a6ee02f4
Keith Stanley <iamstan13y@gmail.com>
2024-05-07 20:38:40 +0200

    Code Refactors.

--- LeafCast/App.xaml.cs
+++ LeafCast/App.xaml.cs
@@ -1,13 +1,11 @@
-﻿using LeafCast.Services;
-
-namespace LeafCast
+﻿namespace LeafCast
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-            
+
 
             MainPage = new NavigationPage(new SplashScreenPage());
         }
