9f3f35232d70e5e2ab8f456487d421e5ce1ad85d
Keith Stanley <iamstan13y@gmail.com>
2024-05-07 20:38:32 +0200

    Code Refactors.

--- LeafCast/Services/HttpService.cs
+++ LeafCast/Services/HttpService.cs
@@ -7,21 +7,21 @@ namespace LeafCast.Services;
 public class HttpService(HttpClient client) : IHttpService
 {
     private readonly HttpClient _client = client;
-	private const string BASE_URL = "https://leafcast-api.onrender.com";
-	
+    private const string BASE_URL = "https://leafcast-api.onrender.com";
+
     public async Task<Result<Dictionary<string, decimal>>> GetTopGradesAsync()
     {
-		try
-		{
+        try
+        {
 
-        var response = await _client.GetAsync($"{BASE_URL}/api/v1/Predictions/top-grades");
-        return await response.ReadContentAs<Result<Dictionary<string, decimal>>>();
-		}
+            var response = await _client.GetAsync($"{BASE_URL}/api/v1/Predictions/top-grades");
+            return await response.ReadContentAs<Result<Dictionary<string, decimal>>>();
+        }
         catch (Exception ex)
-		{
+        {
 
-			throw;
-		}
+            throw;
+        }
     }
 
     public async Task<Result<User>> CreateAccountAsync(CreateAccountRequest request)
